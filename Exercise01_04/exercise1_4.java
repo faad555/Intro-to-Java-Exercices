d19cdd78c10c369489ff88a45ec01ff0496cf437
faad555 <60967843+faad555@users.noreply.github.com>
2020-10-09 16:51:13 +0100

    Add files via upload

---
+++ Exercise01_04/exercise1_4.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class exercise1_4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class exercise1_4
+{
+    public static void main(String[]args)
+    {
+        System.out.println("a  a^2  a^3");
+        System.out.println("1   2    3");
+        System.out.println("2   4    6");
+        System.out.println("3   6    9");
+        System.out.println("4   8    12");
+    }
+}
