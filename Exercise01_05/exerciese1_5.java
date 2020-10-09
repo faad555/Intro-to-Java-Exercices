4a1407435f0c326aa243a6c7e3e5124841c18347
faad555 <60967843+faad555@users.noreply.github.com>
2020-10-09 16:52:44 +0100

    Add files via upload

---
+++ Exercise01_05/exerciese1_5.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class exerciese1_5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class exerciese1_5
+{
+    public static void main(String[]args)
+    {
+        double x;
+        x= ((7.5*2.5)-(1.5*3))/((35.5-2.5));
+        System.out.println(x);
+    }
+}
