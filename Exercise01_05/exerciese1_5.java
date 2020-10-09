f1a5a3f5dd6d5c8dd773c354052876b2117d8c76
faad555 <60967843+faad555@users.noreply.github.com>
2020-10-09 16:56:32 +0100

    Update exerciese1_5.java

--- Exercise01_05/exerciese1_5.java
+++ Exercise01_05/exerciese1_5.java
@@ -1,9 +1,10 @@
 
 /**
- * Write a description of class exerciese1_5 here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ *Write a program that diplays the result of 
+ 7.5*2.5 - 1.5*3
+ ---------------
+   35.2 - 2.5
+   
  */
 public class exerciese1_5
 {
