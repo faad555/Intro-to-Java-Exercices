480c146bf4c9c82fce1cbfeb505919a6ff107629
Fawad Shahzad <60967843+faad555@users.noreply.github.com>
2020-10-16 12:41:41 +0100

    Update exercise2_13.java

--- Exercise02_13/exercise2_13.java
+++ Exercise02_13/exercise2_13.java
@@ -1,9 +1,18 @@
 
 /**
- * Write a description of class exercise2_13 here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ *  Suppose you save $100 each month
+into a savings account with the annual interest rate 5%. Thus, the monthly interest
+rate is 0.05/12 = 0.00417. After the first month, the value in the account
+becomes
+100 * (1 + 0.00417) = 100.417
+After the second month, the value in the account becomes
+(100 + 100.417) * (1 + 0.00417) = 201.252
+After the third month, the value in the account becomes
+(100 + 201.252) * (1 + 0.00417) = 302.507
+and so on.
+Write a program that prompts the user to enter a monthly saving amount and
+displays the account value after the sixth month. (In Exercise 5.30, you will use a
+loop to simplify the code and display the account value for any month.
  */
 public class exercise2_13
 {
